9a4b4cfec41a1d79191eda80e1a24e93873d49e7
debugee <519306795@qq.com>
2024-01-18 21:09:33 +0800

    MSVC_TOOLSET_VERSION

--- include/google/protobuf/protobuf_link.h
+++ include/google/protobuf/protobuf_link.h
@@ -3,29 +3,29 @@
 
 #ifdef _DEBUG
 	#ifdef _DLL
-		#if _MSC_VER == 1800
+		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smdd_libprotobufd.lib")
-		#elif _MSC_VER == 1900
+		#elif MSVC_TOOLSET_VERSION == 140
 			#pragma comment(lib, "v14_smdd_libprotobufd.lib")
 		#endif
 	#else
-		#if _MSC_VER == 1800
+		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smtd_libprotobufd.lib")
-		#elif _MSC_VER == 1900
+		#elif MSVC_TOOLSET_VERSION == 140
 			#pragma comment(lib, "v14_smtd_libprotobufd.lib")
 		#endif
 	#endif
 #else
 	#ifdef _DLL
-		#if _MSC_VER == 1800
+		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smd_libprotobuf.lib")
-		#elif _MSC_VER == 1900
+		#elif MSVC_TOOLSET_VERSION == 140
 			#pragma comment(lib, "v14_smd_libprotobuf.lib")
 		#endif
 	#else
-		#if _MSC_VER == 1800
+		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smt_libprotobuf.lib")
-		#elif _MSC_VER == 1900
+		#elif MSVC_TOOLSET_VERSION == 140
 			#pragma comment(lib, "v14_smt_libprotobuf.lib")
 		#endif
 	#endif
--- include/google/protobuf/protobuf_lite_link.h
+++ include/google/protobuf/protobuf_lite_link.h
@@ -1,10 +1,36 @@
 #ifndef GOOGLE_INCLUDE_GOOGLE_PROTOBUF_LITE_LINK_H_
 #define GOOGLE_INCLUDE_GOOGLE_PROTOBUF_LITE_LINK_H_
 
+
 #ifdef _DEBUG
-#pragma comment(lib, "libprotobuf-lite-mtd.lib")
+	#ifdef _DLL
+		#if MSVC_TOOLSET_VERSION == 120
+			#pragma comment(lib, "v12_smdd_libprotobuf-lited.lib")
+		#elif MSVC_TOOLSET_VERSION == 140
+			#pragma comment(lib, "v14_smdd_libprotobuf-lited.lib")
+		#endif
+	#else
+		#if MSVC_TOOLSET_VERSION == 120
+			#pragma comment(lib, "v12_smtd_libprotobuf-lited.lib")
+		#elif MSVC_TOOLSET_VERSION == 140
+			#pragma comment(lib, "v14_smtd_libprotobuf-lited.lib")
+		#endif
+	#endif
 #else
-#pragma comment(lib, "libprotobuf-lite-mt.lib")
+	#ifdef _DLL
+		#if MSVC_TOOLSET_VERSION == 120
+			#pragma comment(lib, "v12_smd_libprotobuf-lite.lib")
+		#elif MSVC_TOOLSET_VERSION == 140
+			#pragma comment(lib, "v14_smd_libprotobuf-lite.lib")
+		#endif
+	#else
+		#if MSVC_TOOLSET_VERSION == 120
+			#pragma comment(lib, "v12_smt_libprotobuf-lite.lib")
+		#elif MSVC_TOOLSET_VERSION == 140
+			#pragma comment(lib, "v14_smt_libprotobuf-lite.lib")
+		#endif
+	#endif
 #endif
+
 
 #endif  // GOOGLE_INCLUDE_GOOGLE_PROTOBUF_LITE_LINK_H_

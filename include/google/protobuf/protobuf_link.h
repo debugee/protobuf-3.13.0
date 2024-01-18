902d38914caad82d0584fcb4d8c7b8eb22d063e3
debugee <519306795@qq.com>
2024-01-18 18:49:11 +0800

    init

---
+++ include/google/protobuf/protobuf_link.h
@@ -0,0 +1,32 @@
+#pragma once
+
+
+#ifdef _DEBUG
+	#ifdef _DLL
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smdd_libprotobufd.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smdd_libprotobufd.lib")
+		#endif
+	#else
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smtd_libprotobufd.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smtd_libprotobufd.lib")
+		#endif
+	#endif
+#else
+	#ifdef _DLL
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smd_libprotobuf.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smd_libprotobuf.lib")
+		#endif
+	#else
+		#if _MSC_VER == 1800
+			#pragma comment(lib, "v12_smt_libprotobuf.lib")
+		#elif _MSC_VER == 1900
+			#pragma comment(lib, "v14_smt_libprotobuf.lib")
+		#endif
+	#endif
+#endif
---
+++ include/google/protobuf/protobuf_lite_link.h
@@ -0,0 +1,10 @@
+#ifndef GOOGLE_INCLUDE_GOOGLE_PROTOBUF_LITE_LINK_H_
+#define GOOGLE_INCLUDE_GOOGLE_PROTOBUF_LITE_LINK_H_
+
+#ifdef _DEBUG
+#pragma comment(lib, "libprotobuf-lite-mtd.lib")
+#else
+#pragma comment(lib, "libprotobuf-lite-mt.lib")
+#endif
+
+#endif  // GOOGLE_INCLUDE_GOOGLE_PROTOBUF_LITE_LINK_H_

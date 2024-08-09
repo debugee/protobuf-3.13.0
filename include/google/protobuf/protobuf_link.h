0ee5ad047cae790b624fb270f623b8a739855e14
debugee <519306795@qq.com>
2024-08-09 13:10:28 +0800

    link.h

--- include/google/protobuf/protobuf_link.h
+++ include/google/protobuf/protobuf_link.h
@@ -5,13 +5,13 @@
 	#ifdef _DLL
 		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smdd_libprotobufd.lib")
-		#elif MSVC_TOOLSET_VERSION == 140
+		#else
 			#pragma comment(lib, "v14_smdd_libprotobufd.lib")
 		#endif
 	#else
 		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smtd_libprotobufd.lib")
-		#elif MSVC_TOOLSET_VERSION == 140
+		#else
 			#pragma comment(lib, "v14_smtd_libprotobufd.lib")
 		#endif
 	#endif
@@ -19,13 +19,13 @@
 	#ifdef _DLL
 		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smd_libprotobuf.lib")
-		#elif MSVC_TOOLSET_VERSION == 140
+		#else
 			#pragma comment(lib, "v14_smd_libprotobuf.lib")
 		#endif
 	#else
 		#if MSVC_TOOLSET_VERSION == 120
 			#pragma comment(lib, "v12_smt_libprotobuf.lib")
-		#elif MSVC_TOOLSET_VERSION == 140
+		#else
 			#pragma comment(lib, "v14_smt_libprotobuf.lib")
 		#endif
 	#endif
